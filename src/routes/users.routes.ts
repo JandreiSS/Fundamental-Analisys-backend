4f3f8c24b6656c503678d47ed85791b679b7a498
JandreiSS <jandreisst@gmail.com>
2020-07-23 09:20:19 -0300

    chore: User password hash

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -16,7 +16,9 @@ usersRouter.post('/', async (request, response) => {
       password,
     })
 
-    return response.json(user);
+    delete user.password
+
+    return response.json(user)
   } catch (err) {
     return response.status(400).json({ error: err.message })
   }
