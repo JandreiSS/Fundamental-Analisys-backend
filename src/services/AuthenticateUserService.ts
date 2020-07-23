01d7327e4bdffd8878451ff9ef5d1448a6dd7538
JandreiSS <jandreisst@gmail.com>
2020-07-23 09:48:25 -0300

    chore: Generating JSON Web Token

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm'
 import { compare } from 'bcryptjs'
+import { sign } from 'jsonwebtoken'
 
 import User from '../models/User'
 
@@ -10,6 +11,7 @@ interface Request {
 
 interface Response {
   user: User
+  token: string
 }
 
 class AuthenticateUserService {
@@ -32,8 +34,14 @@ class AuthenticateUserService {
 
     // here the user is authenticated
 
+    const token = sign({}, '225006019cfe00e422df28d97205b000', {
+      subject: user.id,
+      expiresIn: '1d',
+    })
+
     return {
       user,
+      token,
     }
   }
 }
